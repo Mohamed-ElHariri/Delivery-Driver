67430f8a036f644e068678be4a47f3eb2b5fffdf
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-10-30 15:49:08 +0200

    Added code to change the car's color when they pick up a package
    To act as feedback for the player to know that they've picked up a package.

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     bool hasPackage = false;
     [SerializeField] float timeToDestroy = 1f;
     void OnCollisionEnter2D(Collision2D other) 
@@ -18,7 +28,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
-            
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, timeToDestroy);
         }
 
@@ -26,6 +36,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
