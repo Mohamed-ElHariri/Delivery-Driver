bc122d937c9a0c836160699e5f1675b9f86e7099
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-10-30 15:04:00 +0200

    Added code to destroy package objects when picked up

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
-
+    [SerializeField] float timeToDestroy = 1f;
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Oye!");
@@ -13,10 +14,12 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
+            
+            Destroy(other.gameObject, timeToDestroy);
         }
 
         if (other.tag == "Customer" && hasPackage)
